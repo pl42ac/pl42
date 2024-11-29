a93782ab70d9bbb05ab506bb8f2981489fb126c5
pl42ac <tdrummond79@gmail.com>
2024-11-29 15:03:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-// List of labels for the quiz
-const labels = ["horn", "head", "abdomen"]; // Add new labels here
-let currentIndex = 0; // Keep track of the current label
+const labels = [
+    "horn", 
+    "hvac unit", 
+    "dynamic brake", 
+    "inertial air filter hatch", 
+    "silencer", 
+    "radiator hatch", 
+    "main reservoir", 
+    "sand box", 
+    "pneumatic rack", 
+    "air compressor", 
+    "truck", 
+    "diesel rack", 
+    "prime mover", 
+    "fuel tank", 
+    "waste fluid tank", 
+    "battery box transformer", 
+    "electrical cabinet", 
+    "toilet", 
+    "vestibule door", 
+    "cab", 
+    "coupler"
+];
+let currentIndex = 0;
 
-// Update the question
 document.getElementById("question").textContent = `Click on: ${labels[currentIndex]}`;
 
 function checkAnswer(selectedPart) {
@@ -11,8 +31,6 @@ function checkAnswer(selectedPart) {
     if (selectedPart === labels[currentIndex]) {
         feedback.textContent = "Correct!";
         feedback.style.color = "green";
-
-        // Move to the next label
         currentIndex++;
         if (currentIndex < labels.length) {
             document.getElementById("question").textContent = `Click on: ${labels[currentIndex]}`;
